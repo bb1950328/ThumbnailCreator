e75f68e444251db3090de1853836befc1d852b74
bb1950328 <bb1950328@gmail.com>
2018-10-06 15:50:37 +0200

    created classes

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from tkinter import *
 
-root = Tk()
-previewFrame = LabelFrame(root)
-previewLabel = Label(previewFrame)
-previewLabel["text"] = "Preview"
-previewFrame["labelwidget"] = previewLabel
-previewFrame.pack()
-previewCanvas = Canvas(previewFrame)
-previewCanvas.pack()
-root.mainloop()
+
+class ThumbnailCreator:
+    def __init__(self):
+        self.root = Tk()
+        self.root.title("ThumbnailCreator")
+        self.preview = ThumbnailCreatorPreview(self.root)
+
+
+class ThumbnailCreatorPreview:
+    def __init__(self, parent):
+        self.root = LabelFrame(parent)
+        self.label = Label(self.root)
+        self.label["text"] = "Preview"
+        self.root["labelwidget"] = self.label
+        self.root.pack()
+        self.canvas = Canvas(self.root)
+        self.root.pack()
+        self.canvas.pack()
+
+
+myCreator = ThumbnailCreator()
+myCreator.root.mainloop()
