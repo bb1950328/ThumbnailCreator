86d2907914747a46495e04aa0cd66b78b0b74200
bb1950328 <bb1950328@gmail.com>
2018-12-05 16:24:55 +0100

    added toolbox.py

--- toolbox.py
+++ toolbox.py
@@ -1,10 +1,22 @@
-from tkinter import Tk
+from tkinter import Tk, Frame, Label, messagebox
+
+from PIL import Image, ImageTk
+
+ICONSIZE = 128
 
 
 class Toolbox:
     def __init__(self):
         self.root = None
         self.toolStarters = []
+
+    def refresh_toolstarters(self):
+        width = self.root.winfo_width()
+        columns = width / ICONSIZE
+        c = r = 0
+        for ts in self.toolStarters:
+            pass
+            # TODO
 
     def run(self):
         self.root = Tk()
@@ -15,6 +27,20 @@ class ToolStarter:
     def __init__(self, tool):
         self.tool = tool
 
-    def generate_frame(self):
-        pass
-        # TODO
+    def generate_frame(self, root):
+        frame = Frame(root)
+        filepath = self.tool.get_icon_path()
+        img = Image.open(filepath).resize((ICONSIZE, ICONSIZE), Image.ANTIALIAS)
+        img = ImageTk.PhotoImage(img)
+        imglabel = Label(frame, image=img)
+        imglabel.pack()
+        namelabel = Label(frame, text=self.tool.get_Name())
+        namelabel.pack()
+        frame.bind("<Button-1>", self.start)
+        return frame
+
+    def start(self):
+        if not self.tool.can_run():
+            messagebox.showerror("Cannot run", "You have too much instances of " + self.tool.get_Name() + " running.")
+            return
+        self.tool.run()
