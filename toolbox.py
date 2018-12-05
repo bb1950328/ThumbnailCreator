5631a9798afc2f477bcf167afc5fea01bfe8bdc0
bb1950328 <bb1950328@gmail.com>
2018-12-05 15:31:39 +0100

    added toolbox.py

--- toolbox.py
+++ toolbox.py
@@ -4,6 +4,17 @@ from tkinter import Tk
 class Toolbox:
     def __init__(self):
         self.root = None
+        self.toolStarters = []
 
     def run(self):
         self.root = Tk()
+        self.root.title("YT Toolbox")
+
+
+class ToolStarter:
+    def __init__(self, tool):
+        self.tool = tool
+
+    def generate_frame(self):
+        pass
+        # TODO
